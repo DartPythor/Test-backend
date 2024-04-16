00298a4b9e22098bfd2dea683eda942716b75c52
Maxim <neverovm28@gmail.com>
2024-04-16 15:26:45 +0500

    feat: edite model filed

--- testbackend/menu/models.py
+++ testbackend/menu/models.py
@@ -1,10 +1,8 @@
 from django.core.exceptions import ValidationError
 from django.db import models
-from django.db.models.expressions import Case, F, When
 from django.urls import reverse
 from django.urls.exceptions import NoReverseMatch
 
-from core.models import NodeAbstract
 from menu.manager import MenuSectionsManager
 
 
@@ -15,17 +13,13 @@ class Menu(models.Model):
         help_text="Укажите название меню.",
         unique=True,
     )
-    slug = models.SlugField(
-        max_length=200,
-        verbose_name="слаг",
-    )
 
     class Meta:
         verbose_name = "меню"
         verbose_name_plural = "меню"
 
 
-class MenuSection(NodeAbstract):
+class MenuSection(models.Model):
     objects = MenuSectionsManager()
 
     name = models.CharField(
@@ -36,7 +30,7 @@ class MenuSection(NodeAbstract):
     )
     url = models.SlugField(
         max_length=200,
-        verbose_name="слаг",
+        verbose_name="ссылка",
     )
     is_named_url = models.BooleanField(
         default=False,
@@ -51,15 +45,18 @@ class MenuSection(NodeAbstract):
         blank=True,
         null=True,
         verbose_name="верхняя секция",
-        help_text="...",
+        help_text="Выберите из какой секции будет выходить секция.",
     )
     menu = models.ForeignKey(
         Menu,
         related_name="sections",
         on_delete=models.CASCADE,
         verbose_name="меню секции",
-        help_text="...",
+        help_text="Выберите из какого меню будет выходить секция.",
     )
+
+    def __str__(self):
+        return self.name
 
     def clean(self):
         super().clean()
@@ -71,38 +68,13 @@ class MenuSection(NodeAbstract):
                     "Вы указали не правильный слаг!",
                 )
         if self.parent and self.parent not in self.menu.sections.all():
-            raise ValidationError("Секция должна быть из укзанного меню!")
+            raise ValidationError("Секция должна быть из указанного меню!")
         if self == self.parent:
             raise ValidationError("Секция не может выходить из самой себя!")
 
-    def save(self, *args, **kwargs):
-        if self.parent:
-            add_child(self.parent, self)
-        super().save(*args, **kwargs)
-
-    class Meta(NodeAbstract.Meta):
+    class Meta:
         verbose_name = "секция меню"
         verbose_name_plural = "секции меню"
-
-
-def add_child(parent: MenuSection, child: MenuSection):
-    MenuSection.objects.filter(
-        right__gte=parent.right,
-        menu=parent.menu,
-    ).update(
-        left=Case(
-            When(
-                left__gt=parent.right,
-                then=F("left") + 2,
-            ),
-            default=F("left"),
-            output_field=models.PositiveIntegerField(),
-        ),
-        right=F("right") + 2,
-    )
-    child.left = parent.right
-    child.right = parent.right + 1
-    child.level = parent.level + 1
 
 
 __all__ = ()
