81a19cdb94f9c93924f5c48000eaec65d871183a
Maxim <neverovm28@gmail.com>
2024-04-16 15:31:02 +0500

    feat: Edit manager method for get all tree

--- testbackend/menu/manager.py
+++ testbackend/menu/manager.py
@@ -2,15 +2,14 @@ from django.db.models import Manager
 
 
 class MenuSectionsManager(Manager):
-    def get_all_tree(self, node):
+    def get_all_tree(self, menu_name):
         return (
             super()
             .get_queryset()
             .filter(
-                left__gte=node.left,
-                right__lte=node.right,
+                menu__name=menu_name,
             )
-        )
+        ).values()
 
 
 __all__ = ()
