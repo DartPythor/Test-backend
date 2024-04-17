22900d65af86516924601ea31a84e154672f8934
Maxim <neverovm28@gmail.com>
2024-04-17 18:52:47 +0500

    feat: Add new urls

--- testbackend/menu/urls.py
+++ testbackend/menu/urls.py
@@ -1,12 +1,22 @@
 from django.urls import path
 
-from menu.views import ContactSeoView, ContactView, LocationView, MenuView
+from menu.views import MenuView
 
 app_name = "menu"
 urlpatterns = [
     path("", MenuView.as_view(), name="home"),
-    path("contacts/", ContactView.as_view(), name="contacts"),
-    path("contacts/ceo/", ContactSeoView.as_view(), name="seo"),
-    path("location/", LocationView.as_view(), name="location"),
-    path("contacts/max", LocationView.as_view(), name="max"),
+    path("contacts/", MenuView.as_view(), name="contacts"),
+    path("contacts/ceo/", MenuView.as_view(), name="ceo"),
+    path("contacts/manager/", MenuView.as_view(), name="manager"),
+    path("contacts/leads/", MenuView.as_view(), name="leads"),
+    path("contacts/leads/python/", MenuView.as_view(), name="python"),
+    path("contacts/leads/goland/", MenuView.as_view(), name="golang"),
+    path("location/", MenuView.as_view(), name="location"),
+    path("location/office/", MenuView.as_view(), name="office"),
+    path("menu/", MenuView.as_view(), name="menu"),
+    path("menu/account/", MenuView.as_view(), name="account"),
+    path("menu/friends/", MenuView.as_view(), name="friends"),
+    path("menu/news/", MenuView.as_view(), name="news"),
+    path("menu/news/hot/", MenuView.as_view(), name="news_hot"),
+    path("menu/news/ice/", MenuView.as_view(), name="news_ice"),
 ]
